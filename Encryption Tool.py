149898e072c23a30bce5382ee871fa3213cee0a5
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2025-04-07 23:00:48 +0100

    Password entry box created and working

--- Encryption Tool.py
+++ Encryption Tool.py
@@ -13,6 +13,11 @@ def main_screen():
     text1=Text(font="Robote 20", bg="black", relief=GROOVE, wrap=WORD, bd=0)
     text1.place(x=10, y=50, width=335,height=100)
 
+    Label(text="Enter secret key for encryption and decryption",fg="white", font=("calbri", 13)).place(x=10, y=170)
+
+    code=StringVar()
+    Entry(textvariable=code, width=24,bd=0,font=("arial", 25),show="*").place(x=10, y=200)
+
     screen.mainloop()
 
 main_screen()
