5e7c63fb036a8b3e6f002bb7ce7d4de261a13378
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2025-04-07 23:22:44 +0100

    Encryption is working

--- Encryption Tool.py
+++ Encryption Tool.py
@@ -3,7 +3,38 @@ from tkinter import messagebox
 import base64
 import os
 
+
+def decrypt():
+    print("")
+
+def encrypt():
+    password=code.get()
+
+    if password == "1234":
+        screen1=Toplevel(screen)
+        screen1.title("encryption")
+        screen1.geometry("400x200")
+        screen1.configure(bg="#ed3833")
+
+        message=text1.get(1.0,END)
+        encode_message=message.encode("ascii")
+        base64_bytes=base64.b64encode(encode_message)
+        encrypt=base64_bytes.decode("ascii")
+
+        Label(screen1,text="ENCRYPT",font="arial",fg="white",bg="#ed3833").place(x=10, y=0)
+        text2=Text(screen1,font="Robote 10",bg="black",relief=GROOVE,wrap=WORD,bd=0)
+        text2.place(x=10,y=40,width=380,height=150)
+
+        text2.insert(END,encrypt)
+
+
 def main_screen():
+
+    global screen
+    global code 
+    global text1
+
+
     screen=Tk()
     screen.geometry("375x398")
 
