8d83266128e8eabead376657b8b1022c1d73f21e
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2025-04-07 22:52:36 +0100

    Main screen was created and entry box is working

--- Encryption Tool.py
+++ Encryption Tool.py
@@ -2,3 +2,17 @@ from tkinter import *
 from tkinter import messagebox
 import base64
 import os
+
+def main_screen():
+    screen=Tk()
+    screen.geometry("375x398")
+
+    screen.title("PctApp")
+
+    Label(text="Enter text for encryption", fg="white", font=("calbri",13)).place(x=10,y=10)
+    text1=Text(font="Robote 20", bg="black", relief=GROOVE, wrap=WORD, bd=0)
+    text1.place(x=10, y=50, width=335,height=100)
+
+    screen.mainloop()
+
+main_screen()
